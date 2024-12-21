16f26566455add2e369eae84ae655a125b652b15
MeexReay <meexreay@gmail.com>
2024-12-22 01:36:43 +0300

    build client updateeeee

--- client/build.sh
+++ client/build.sh
@@ -7,3 +7,9 @@ cat dest/network.js >> dest/script.js
 cat dest/player.js >> dest/script.js
 cat dest/core.js >> dest/script.js
 terser dest/script.js -o dest/script.min.js --compress --mangle
+rm dest/script.js
+rm dest/main.js
+rm dest/block.js
+rm dest/network.js
+rm dest/player.js
+rm dest/core.js
